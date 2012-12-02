ca393d1503b49017b2256d2e64afb703ee2c5875
yotamfra@gmail.com <yotamfra@gmail.com@95543dbd-011b-5fc7-f12f-8ce24854aa44>
2012-12-02 23:05:24 +0000

    PA2 Lexer is up and running

--- branches/IC_COMPILER/PA2/src/IC/Parser/LexicalError.java
+++ branches/IC_COMPILER/PA2/src/IC/Parser/LexicalError.java
@@ -1,8 +1,35 @@
 package IC.Parser;
 
+/**
+ * This is the LexicalError class. This exception is thrown
+ * every time we get an error from the Lexer.
+ * 
+ * @author Eidan
+ *
+ */
 public class LexicalError extends Exception
 {
-    public LexicalError(String message) {
-     // do something
+	private int linen;
+	private String msg;
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 6212043046195094107L;
+	
+	public LexicalError(String message) {} // to avoid compilation errors. do not remove!
+	
+	public LexicalError(String message, int linen) {
+		super(linen+": "+"Lexical error: "+message);
+		this.msg = message;
+		this.linen = linen;
     }
+	
+	public int getLine() { return linen; }
+	public String getMsg() { return msg; }
+	
+	@ Override
+	public String toString(){
+		return super.getMessage();
+	}
 }
+
